9e089677d2039464cc34b04c84665d4329177436
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-05-13 17:17:03 +0300

    Remove unused imports from Cover and SizesTable components

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import React from "react";
-import sohag from "../../public/sohag-foe.png";
 
 export default function Cover() {
   return <div className=" relative rounded-l-lg "></div>;
